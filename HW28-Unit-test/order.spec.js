010f0fd8da86aeb4d2d4fafc85ac95964a570b59
Maksym Kozlov <54378060+SkiF-ex@users.noreply.github.com>
2020-08-30 12:21:21 +0300

    add

---
+++ HW28-Unit-test/order.spec.js
@@ -0,0 +1,56 @@
+describe('order.js', () => {
+  let order;
+  let spyTotalPrice;
+
+  beforeEach(() => {
+    order = new Order(); 
+    spyTotalPrice = spyOnProperty(order, 'totalPrice').and.callThrough();
+    order.addPizza(new Pizza (['olives'], 'large'));
+  });
+
+  afterEach(() => {
+    order.removePizza('Pizza');
+  })
+
+  it('should initialize pizzas array', () => {
+    expect(order.pizzas).toBeArray();
+  });
+
+  it('should push pizza in array', () => {
+    expect(order.pizzas).toBeArrayOfSize(1);
+  });
+
+  it('shoud contain objects in pizzas array', () => {
+    expect(order.pizzas).toBeArrayOfObjects();
+  })
+
+  it('should remove passed pizza from pizzas', () => {
+    order.pizzas = ['pizza'];
+    order.removePizza('pizza');
+    expect(order.pizzas).toBeArrayOfSize(0);
+  });
+
+  describe('totalPrice get', () => {
+    afterEach(() => {
+      order.removePizza('Pizza');
+    });
+
+    it('should calculate total price', () => {
+      expect(order.totalPrice).toBe(0.6);
+    });
+
+    it(`should throw error 'Pizza can't cost 0 USD'`, () => {
+      const test = {pizzaPrice: 0, toppings: 'olives', size: 'large'}
+      order.addPizza(test);
+
+      expect(() => order.totalPrice).toThrowError(`Pizza can't cost 0 USD`);
+      order.removePizza(test);
+    });
+
+    it(`should throw error message 'Pizza must have a price'`, () => {
+      this.pizzas = ['pizza'];
+
+      expect(spyTotalPrice).toThrowError(`Pizza must have a price`);
+    });
+  });
+});
---
+++ HW28-Unit-test/pizza.spec.js
@@ -0,0 +1,59 @@
+describe('pizza.js', () => {
+  let pizza;
+  let spyPizzaPrice;
+
+  beforeEach(() => {
+    pizza = new Pizza(); 
+    spyPizzaPrice = spyOnProperty(pizza, 'pizzaPrice').and.callThrough();
+  });
+
+  afterEach(() => {
+    size[this.size] = 0;
+  });
+  
+  describe('pizzaPrice get', () => {
+    it('should throw error(Size can`t find)', () => {
+      expect(spyPizzaPrice).toThrowError(`Size can't find`);
+    });
+  
+    it('should calculate pizza price', () => {
+      const spyToppingsPrice = spyOnProperty(pizza, 'toppingsPrice').and.returnValue(2);
+      size[this.size] = 2;
+
+      expect(pizza.pizzaPrice).toBe(4);
+      expect(spyPizzaPrice).toHaveBeenCalled();
+    });
+  });
+
+  describe('toppingsPrice get', () => {
+    let spyToppingsPrice;
+
+    beforeEach(() => {
+      spyToppingsPrice = spyOnProperty(pizza, 'toppingsPrice').and.callThrough();
+    });
+
+    afterEach(() => {
+      pizza.toppings = [];
+      this.toppings = [];
+    });
+
+    it('should calculate toppings price', () => {
+      pizza.toppings = ['olives', 'mushrooms'];
+
+      expect(pizza.toppingsPrice).toBe(0.55);
+      expect(spyToppingsPrice).toHaveBeenCalled();
+    });
+
+    it('should throw error `Topping ${i} can`t find`', () => {
+      this.toppings = ['fake'];
+
+      expect(spyToppingsPrice).toThrowError(`Topping fake can't find`);
+    });
+
+    xit('should throw error `Toppings can`t find`', () => {
+      this.toppings = undefined;
+      !this.toppings
+      expect(spyToppingsPrice).toThrowError(`Toppings can't find`);
+    });
+  });
+})
